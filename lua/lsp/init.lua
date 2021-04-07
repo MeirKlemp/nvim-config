4a910ed867fc860c970caba76a78a03baadd3a3a
Meir Klemfner <meirk251@outlook.com>
2021-04-07 09:26:14 +0300

    Change lsp diagnostic signs. Map code action to ^h
    I mapped code action from `ca` because I am using the default map of
    `ca`.

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,12 @@
+vim.fn.sign_define("LspDiagnosticsSignError",
+                   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"})
+vim.fn.sign_define("LspDiagnosticsSignWarning",
+                   {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"})
+vim.fn.sign_define("LspDiagnosticsSignHint",
+                   {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"})
+vim.fn.sign_define("LspDiagnosticsSignInformation",
+                   {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"})
+
 local keymap = vim.api.nvim_set_keymap
 local options = {noremap = true, silent = true}
 
@@ -8,7 +17,7 @@ keymap('n', 'gr', "<cmd>lua vim.lsp.buf.references()<CR>", options)
 keymap('n', 'gh', ":Lspsaga lsp_finder<CR>", options)
 keymap('n', 'K', ":Lspsaga hover_doc<CR>", options)
 keymap('n', 'cr', ":Lspsaga rename<CR>", options)
-keymap('n', 'ca', ":Lspsaga code_action<CR>", options)
+keymap('n', '<C-h>', ":Lspsaga code_action<CR>", options)
 keymap('n', '<C-p>', ":Lspsaga diagnostic_jump_prev<CR>", options)
 keymap('n', '<C-n>', ":Lspsaga diagnostic_jump_next<CR>", options)
 keymap('n', '<C-d>', "<cmd>lua require'lspsaga.action'.smart_scroll_with_saga(-1)<CR>", options)
