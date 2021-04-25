583ed605935997af4591e321cbc486a24fa5a837
Meir Klemfner <meirk251@outlook.com>
2021-04-25 20:03:32 +0300

    feat: keymap f11 to neovide fullscreen.

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -14,3 +14,8 @@ keymap('n', 'gB', ':bprev<CR>', {noremap = true, silent = true})
 -- move selected line up and down in visual mode with J and K
 keymap('x', 'K', ":move '<-2<CR>gv-gv", {noremap = true, silent = true})
 keymap('x', 'J', ":move '>+1<CR>gv-gv", {noremap = true, silent = true})
+--
+-- toggle full screen for neovide
+if vim.g.neovide == true then
+    keymap('n', '<F11>', ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
+end
